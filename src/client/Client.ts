e99d2f685d660ed2fd1c01ccccbe9f377ff50a2a
Milo123459 <milo5huntington@gmail.com>
2020-10-08 09:59:38 +0100

    lint

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,10 +1,10 @@
-import consola, { Consola } from "consola";
+import consola, { Consola } from 'consola';
 import { Client, Collection, Intents } from 'discord.js';
-import glob from "glob";
-import { promisify } from "util";
-import * as mongoose from "mongoose";
-import { join } from "path";
-import { Command } from "../interfaces/Command";
+import glob from 'glob';
+import { promisify } from 'util';
+import * as mongoose from 'mongoose';
+import { join } from 'path';
+import { Command } from '../interfaces/Command';
 const globPromise = promisify(glob);
 class BitClient extends Client {
 	private logger: Consola = consola;
@@ -19,16 +19,20 @@ class BitClient extends Client {
 			messageCacheMaxSize: 200,
 			messageSweepInterval: 180,
 		});
-	};
+	}
 	public async start(token: string) {
 		this.login(token).catch((e) => this.logger.error(e));
 		this.logger.info(`Loading commands..`);
-		const commandFiles: string[] = await globPromise(`${__dirname}/../commands/**/*{.js,.ts}`);
-		const eventFiles: string[] = await globPromise(`${__dirname}/../events/**/*{.js,.ts}`);
-		commandFiles.map(async(cmdFile: string) => {
-			const cmd = await import(cmdFile) as Command;
+		const commandFiles: string[] = await globPromise(
+			`${__dirname}/../commands/**/*{.js,.ts}`,
+		);
+		const eventFiles: string[] = await globPromise(
+			`${__dirname}/../events/**/*{.js,.ts}`,
+		);
+		commandFiles.map(async (cmdFile: string) => {
+			const cmd = (await import(cmdFile)) as Command;
 			this.commands.set(cmd.name, cmd);
-			if(cmd.aliases) {
+			if (cmd.aliases) {
 				cmd.aliases.map((alias: string) => this.aliases.set(alias, cmd.name));
 			}
 		});
--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -1,13 +1,13 @@
-import { Message } from "discord.js";
-import { BitClient } from "../client/Client";
+import { Message } from 'discord.js';
+import { BitClient } from '../client/Client';
 
 export interface RunFunction {
-    (client: BitClient, message: Message, args: string[]): Promise<void>
+	(client: BitClient, message: Message, args: string[]): Promise<void>;
 }
 export interface Command {
-    name: string;
-    run: RunFunction;
-    aliases?: string[];
-    description?: string;
-    cooldown?: string|number
+	name: string;
+	run: RunFunction;
+	aliases?: string[];
+	description?: string;
+	cooldown?: string | number;
 }
